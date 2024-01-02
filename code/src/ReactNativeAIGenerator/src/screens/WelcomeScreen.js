f8615d3998486429528069322a7715d0f2c236a1
szhao23 <stevenzhao456@gmai.com>
2024-01-02 10:39:10 -0600

    Welcome Screen design complete, navigation to Home Screen added

--- code/src/ReactNativeAIGenerator/src/screens/WelcomeScreen.js
+++ code/src/ReactNativeAIGenerator/src/screens/WelcomeScreen.js
@@ -1,23 +1,44 @@
-import {View, Text, SafeAreaView, Image} from 'react-native';
+import {View, Text, SafeAreaView, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {useNavigation} from '@react-navigation/native';
 
 export default function WelcomeScreen() {
+  // Redirect to HomeScreen
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView className="flex-1 flex justify-around bg-white">
       <View className="space-y-2">
-        <Text className="text-center text-4xl font-bold text-gray-700">
+        <Text
+          style={{fontSize: wp(10)}}
+          className="text-center text-4xl font-bold text-gray-700">
           Personal AI
         </Text>
-        <Text className="text-center tracking-wider text-gray-500 font-semibold">
+        <Text
+          style={{fontSize: wp(4)}}
+          className="text-center tracking-wider text-gray-500 font-semibold">
           The future has arrived, powered by AI
         </Text>
       </View>
       <View className="flex-row justify-center">
         <Image
           source={require('../../assets/images/welcome.png')}
-          className="w-72 h-72"
+          style={{width: wp(75), height: wp(75)}}
         />
       </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Home')}
+        className="bg-emerald-600 mx-5 p-4 rounded-2xl">
+        <Text
+          style={{fontSize: wp(6)}}
+          className="text-center font-bold text-white text-2xl">
+          Get Started
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
